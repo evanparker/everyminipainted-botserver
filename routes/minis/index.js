c12350952d63f40a486273f26b5b4e5309f14949
Evan Parker <parkevan@gmail.com>
2025-04-14 10:26:06 -0700

    add thumbnail image

--- routes/minis/index.js
+++ routes/minis/index.js
@@ -2,6 +2,8 @@ var express = require("express");
 const { getMini } = require("../../services/mini");
 var router = express.Router();
 
+const cloudName = process.env.VITE_CLOUD_NAME || "ddl3gn9nh";
+
 /* GET minis. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Every Mini Painted" });
@@ -11,10 +13,11 @@ router.get("/", function (req, res, next) {
 router.get("/:id", async function (req, res, next) {
   try {
     const mini = await getMini(req.params.id);
-
+    const thumbnailImageURL = `https://res.cloudinary.com/${cloudName}/image/upload/f_auto/q_auto:good/c_pad,h_400,w_400/${mini.images[0].cloudinaryPublicId}`;
     res.render("minis/mini", {
       title: `EMP - ${mini.name}`,
       description: mini.description,
+      thumbnailImageURL,
     });
   } catch (e) {
     console.log(e.message);
@@ -23,3 +26,5 @@ router.get("/:id", async function (req, res, next) {
 });
 
 module.exports = router;
+
+// https://res.cloudinary.com/ddl3gn9nh/image/upload/f_auto/q_auto:good/c_pad,h_400,w_400/sqjjchoujefu9ppnkujv?_a=DAJCyJATZAA0
