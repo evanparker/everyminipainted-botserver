63437657c2a1b3f807cd6ae763ab446be8a2ede2
Evan Parker <parkevan@gmail.com>
2025-04-14 09:43:51 -0700

    Let's try some dynamic data

--- services/apiClient.js
+++ services/apiClient.js
@@ -1,75 +1,71 @@
-// // credit to https://profy.dev/article/react-architecture-api-client
+// credit to https://profy.dev/article/react-architecture-api-client
 
-// import { createRequire } from "module";
-// const require = createRequire(import.meta.url);
-// const dotenv = require("dotenv").config();
+class APIClient {
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
 
-// class APIClient {
-//   constructor(baseURL) {
-//     this.baseURL = baseURL;
-//   }
+  async request(url, options) {
+    console.log(`${this.baseURL}${url}`);
+    const response = await fetch(`${this.baseURL}${url}`, options);
+    if (!response.ok) {
+      const error = new Error("HTTP Error");
+      error.status = response.status;
+      // error.response = await response.json();
+      throw error;
+    }
+    return response.json();
+  }
 
-//   async request(url, options) {
-//     console.log(`${this.baseURL}${url}`);
-//     const response = await fetch(`${this.baseURL}${url}`, options);
-//     if (!response.ok) {
-//       const error = new Error("HTTP Error");
-//       error.status = response.status;
-//       // error.response = await response.json();
-//       throw error;
-//     }
-//     return response.json();
-//   }
+  get(url) {
+    // const token = JSON.parse(localStorage.getItem("token"));
+    return this.request(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        // authorization: "Bearer " + token,
+      },
+    });
+  }
 
-//   get(url) {
-//     // const token = JSON.parse(localStorage.getItem("token"));
-//     return this.request(url, {
-//       method: "GET",
-//       headers: {
-//         "Content-Type": "application/json",
-//         "Access-Control-Allow-Origin": "*",
-//         // authorization: "Bearer " + token,
-//       },
-//     });
-//   }
+  // post(url, data) {
+  //   const token = JSON.parse(localStorage.getItem("token"));
+  //   return this.request(url, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Access-Control-Allow-Origin": "*",
+  //       authorization: "Bearer " + token,
+  //     },
+  //     body: JSON.stringify(data),
+  //   });
+  // }
 
-//   // post(url, data) {
-//   //   const token = JSON.parse(localStorage.getItem("token"));
-//   //   return this.request(url, {
-//   //     method: "POST",
-//   //     headers: {
-//   //       "Content-Type": "application/json",
-//   //       "Access-Control-Allow-Origin": "*",
-//   //       authorization: "Bearer " + token,
-//   //     },
-//   //     body: JSON.stringify(data),
-//   //   });
-//   // }
+  // put(url, data) {
+  //   const token = JSON.parse(localStorage.getItem("token"));
+  //   return this.request(url, {
+  //     method: "PUT",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Access-Control-Allow-Origin": "*",
+  //       authorization: "Bearer " + token,
+  //     },
+  //     body: JSON.stringify(data),
+  //   });
+  // }
 
-//   // put(url, data) {
-//   //   const token = JSON.parse(localStorage.getItem("token"));
-//   //   return this.request(url, {
-//   //     method: "PUT",
-//   //     headers: {
-//   //       "Content-Type": "application/json",
-//   //       "Access-Control-Allow-Origin": "*",
-//   //       authorization: "Bearer " + token,
-//   //     },
-//   //     body: JSON.stringify(data),
-//   //   });
-//   // }
+  // delete(url) {
+  //   const token = JSON.parse(localStorage.getItem("token"));
+  //   return this.request(url, {
+  //     method: "DELETE",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Access-Control-Allow-Origin": "*",
+  //       authorization: "Bearer " + token,
+  //     },
+  //   });
+  // }
+}
 
-//   // delete(url) {
-//   //   const token = JSON.parse(localStorage.getItem("token"));
-//   //   return this.request(url, {
-//   //     method: "DELETE",
-//   //     headers: {
-//   //       "Content-Type": "application/json",
-//   //       "Access-Control-Allow-Origin": "*",
-//   //       authorization: "Bearer " + token,
-//   //     },
-//   //   });
-//   // }
-// }
-
-// export const apiClient = new APIClient(process.env.VITE_API_URL);
+export const apiClient = new APIClient(process.env.VITE_API_URL);
